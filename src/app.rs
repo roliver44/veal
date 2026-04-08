37239f5eaecbb02ec62e82622f2a2c9fd2505730
Ryan Doko <doko5879@vandals.uidaho.edu>
2026-04-08 14:54:04 -0700

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -2,5 +2,9 @@ slint::include_modules!();
 
 pub fn run() {
     let ui = App::new().unwrap();
+
+    crate::ui::state::init_state(&ui);
+    crate::ui::bindings::setup_bindings(&ui);
+
     ui.run().unwrap();
 }
